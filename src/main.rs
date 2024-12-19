6a16c9384feb2c4ea8e813ced2dfd32c1cbbb789
Driwell <driwell@tuta.io>
2024-12-19 22:49:28 +0000

    add pronouns and greet people

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,54 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
+#[derive(Component)]
+enum Pronoun {
+    He,
+    She,
+    They,
+}
+
 #[derive(Bundle)]
 struct PersonBundle {
     kind: Person,
     name: Name,
+    pronouns: Pronoun,
 }
 
 fn add_citizen(mut commands: Commands) {
     commands.spawn(PersonBundle {
         kind: Person,
         name: Name("Foo".to_string()),
+        pronouns: Pronoun::She,
+    });
+    commands.spawn(PersonBundle {
+        kind: Person,
+        name: Name("Bar".to_string()),
+        pronouns: Pronoun::He,
+    });
+    commands.spawn(PersonBundle {
+        kind: Person,
+        name: Name("Baz".to_string()),
+        pronouns: Pronoun::They,
     });
 }
 
-fn welcome_citizens(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("Welcome {}!", name.0);
+fn welcome_citizens(query: Query<(&Name, &Pronoun), With<Person>>) {
+    for (name, pronoun) in &query {
+        let pronoun = match pronoun {
+            Pronoun::He => "Mr.",
+            Pronoun::She => "Ms.",
+            Pronoun::They => "",
+        };
+
+        println!(
+            "Welcome {}{}!",
+            if pronoun.is_empty() {
+                pronoun.to_string()
+            } else {
+                format!("{pronoun} ")
+            },
+            name.0
+        );
     }
 }
