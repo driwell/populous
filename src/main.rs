e445ac77d71e5cc8cdfe0336e7dd60b1fe30f9e5
Driwell <driwell@tuta.io>
2024-12-19 22:16:06 +0000

    add citizens

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_systems(Startup, greet).run();
+    App::new()
+        .add_systems(Startup, (add_citizen, welcome_citizens).chain())
+        .run();
 }
 
-fn greet() {
-    println!("hello");
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Bundle)]
+struct PersonBundle {
+    kind: Person,
+    name: Name,
+}
+
+fn add_citizen(mut commands: Commands) {
+    commands.spawn(PersonBundle {
+        kind: Person,
+        name: Name("Foo".to_string()),
+    });
+}
+
+fn welcome_citizens(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Welcome {}!", name.0);
+    }
 }
